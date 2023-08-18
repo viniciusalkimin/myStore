e5bfdce6a876972de866129f3f1c5e8408e47d4e
viniciusalkimin <viniciusg_alkimin@hotmail.com>
2023-08-18 17:53:54 -0300

    Configuration of TypeORM on project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { ProductModule } from './product/product.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DbConfigService } from './config/db.config.service';
+import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
-  imports: [UserModule, ProductModule],
+  imports: [UserModule, ProductModule, TypeOrmModule.forRootAsync({
+    useClass: DbConfigService,
+    inject: [DbConfigService]
+  }), ConfigModule.forRoot({
+    isGlobal: true
+  })],
   controllers: [],
   providers: [],
 })
