a504b9f7bdb17dd30505c437c32a113931c29bf2
viniciusalkimin <viniciusg_alkimin@hotmail.com>
2023-11-23 23:06:20 -0300

    Add intensPedido to Pedido in layer service

--- src/pedido/pedido.service.ts
+++ src/pedido/pedido.service.ts
@@ -2,30 +2,37 @@ import { Injectable } from '@nestjs/common';
 import { CreatePedidoDto } from './dto/create-pedido.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { PedidoEntity } from './pedido.entity';
-import { UserEntity } from 'src/user/user.entity';
-import { Repository } from "typeorm";
+import { UserEntity } from '../user/user.entity';
+import { In, Repository } from "typeorm";
 import { StatusPedido } from './enum/statuspedido.enum';
 import { ItemPedidoEntity } from './itempedido.entity';
+import { ProductEntity } from '../product/product.entity';
 
 @Injectable()
 export class PedidoService {
   constructor(
     @InjectRepository(PedidoEntity) private readonly pedidoRepository: Repository<PedidoEntity>,
     @InjectRepository(UserEntity) private readonly userRepository: Repository<UserEntity>,
+    @InjectRepository(ProductEntity) private readonly produtoRepository: Repository<ProductEntity>,
     ){}
 
     async cadastraPedido(usuarioId: string, dadosPedido: CreatePedidoDto) {
       const usuario = await this.userRepository.findOneBy({id: usuarioId});
+      const produtosIds = dadosPedido.itensPedido.map((itemPedido) => itemPedido.produtoId);
       const pedidoEntity = new PedidoEntity();
+      const produtosRelacionados = await this.produtoRepository.findBy({id:In(produtosIds)});
 
       pedidoEntity.status = StatusPedido.EM_PROCESSAMENTO;
       pedidoEntity.user = usuario;
 
-        const itensPedidoEntidades = dadosPedido.itensPedido.map((itemPedido) => {
-        const itemPedidoEntity = new ItemPedidoEntity();
-        itemPedidoEntity.quantidade = 10;
-        itemPedidoEntity.precoVenda = itemPedido.quantidade;
-        return itemPedidoEntity;
+      const itensPedidoEntidades = dadosPedido.itensPedido.map((itemPedido) => {
+      const produtoRelacionado = produtosRelacionados.find((produto) => produto.id === itemPedido.produtoId);
+      const itemPedidoEntity = new ItemPedidoEntity();
+      itemPedidoEntity.produto = produtoRelacionado;
+      itemPedidoEntity.quantidade = itemPedido.quantidade; 
+      itemPedidoEntity.precoVenda = produtoRelacionado.value;
+      itemPedidoEntity.produto.quantity -= itemPedido.quantidade;
+      return itemPedidoEntity;
       });
 
       const valorTotal = itensPedidoEntidades.reduce((total, item) => {
